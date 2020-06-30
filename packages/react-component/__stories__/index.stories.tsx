37c05a3f58163fb160ba29a90223f1bbfe618d45
Olivier Hoareau <oha@greenberets.io>
2020-06-30 09:10:31 +0200

    react-component: add stories

--- packages/react-component/__stories__/index.stories.tsx
+++ packages/react-component/__stories__/index.stories.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import Component from '../src';
-//import {action} from '@storybook/addon-actions';
-import {withKnobs} from '@storybook/addon-knobs';
+import {withKnobs, text} from '@storybook/addon-knobs';
+import {action} from "@storybook/addon-actions";
+import component from "../src";
+
+const DummyComponent = component({
+    root: {
+        backgroundColor: props => props.color,
+        color: 'white',
+    },
+}, ({classes = {}, children, ...props}) => (
+    <div className={classes.root} {...props}>{children}</div>
+));
 
 export default {
     title: 'Component',
-    component: Component,
+    component: DummyComponent,
     decorators: [withKnobs],
 }
 
-export const basic = () => <Component />;
+export const basic = () => (
+    <DummyComponent color={text('Color', 'pink')} onClick={action('click')}>
+        {text('Content', 'THIS IS THE CONTENT')}
+    </DummyComponent>
+);
